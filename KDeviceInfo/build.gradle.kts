4d0d98a2d868911aa382da044497c3fb85180914
Swapnil Musale <swapnilmusale19@gmail.com>
2024-01-26 12:02:48 +0530

    Initial commit

---
+++ KDeviceInfo/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    id("convention.publication")
+}
+
+group = "com.devx.kdeviceinfo"
+version = "1.0"
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release")
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "KDeviceInfo"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations["main"].compilerOptions.options.freeCompilerArgs.add("-Xexport-kdoc")
+    }
+}
+
+android {
+    namespace = "com.devx.kdeviceinfo"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "KMP-DeviceInfo"
+include(":KDeviceInfo")
+includeBuild("convention-plugins")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
