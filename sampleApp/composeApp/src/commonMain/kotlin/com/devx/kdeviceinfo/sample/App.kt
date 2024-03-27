3f38a3654bfd5749005baf3f8c786bf431c44823
Swapnil Musale <swapnilmusale19@gmail.com>
2024-03-27 10:21:26 +0530

    sampleApp : display deviceInfo in section

--- sampleApp/composeApp/src/commonMain/kotlin/com/devx/kdeviceinfo/sample/App.kt
+++ sampleApp/composeApp/src/commonMain/kotlin/com/devx/kdeviceinfo/sample/App.kt
@@ -1,7 +1,10 @@
 package com.devx.kdeviceinfo.sample
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -11,7 +14,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devx.kdeviceinfo.DeviceInfoXState
 import com.devx.kdeviceinfo.model.android.AndroidInfo
 import com.devx.kdeviceinfo.model.ios.IosInfo
@@ -47,21 +52,32 @@ private fun ShowAndroidDeviceInfo(androidInfo: AndroidInfo) {
     val verticalScrollState = rememberScrollState()
 
     Column(modifier = Modifier.verticalScroll(state = verticalScrollState)) {
-        Text(text = "Language Code : ${androidInfo.locale.languageCode}")
-        Text(text = "Region : ${androidInfo.locale.region}")
+        // App Info
+        Text(text = "App Info", style = TextStyle(fontSize = 20.sp))
+        Text(text = "App Name : ${androidInfo.appName}")
         Text(text = "Version Code : ${androidInfo.versionCode}")
         Text(text = "Version Name : ${androidInfo.versionName}")
+        Text(text = "Package Name : ${androidInfo.packageName}")
+        Spacer(modifier = Modifier.fillMaxWidth().height(height = 20.dp))
+
+        // Device Info
+        Text(text = "Device Info", style = TextStyle(fontSize = 20.sp))
         Text(text = "Device : ${androidInfo.device}")
         Text(text = "SdkInt : ${androidInfo.version.sdkInt}")
         Text(text = "Release : ${androidInfo.version.release}")
-        Text(text = "App Name : ${androidInfo.appName}")
-        Text(text = "Package Name : ${androidInfo.packageName}")
         Text(text = "CodeName : ${androidInfo.version.codeName}")
         Text(text = "Board : ${androidInfo.board}")
         Text(text = "IsPhysicalDevice : ${androidInfo.isPhysicalDevice}")
         Text(text = "Manufacturer : ${androidInfo.manufacturer}")
         Text(text = "Model : ${androidInfo.model}")
         Text(text = "Orientation : ${androidInfo.deviceOrientation.getDeviceOrientation()}")
+        Spacer(modifier = Modifier.fillMaxWidth().height(height = 20.dp))
+
+        // Locale
+        Text(text = "Locale Info", style = TextStyle(fontSize = 20.sp))
+        Text(text = "Language Code : ${androidInfo.locale.languageCode}")
+        Text(text = "Region : ${androidInfo.locale.region}")
+        Spacer(modifier = Modifier.fillMaxWidth().height(height = 20.dp))
     }
 }
 
@@ -70,18 +86,30 @@ private fun ShowIosDeviceInfo(iosInfo: IosInfo) {
     val verticalScrollState = rememberScrollState()
 
     Column(modifier = Modifier.verticalScroll(state = verticalScrollState)) {
-        Text(text = "Language Code : ${iosInfo.locale.languageCode}")
-        Text(text = "Region : ${iosInfo.locale.region}")
+
+        // App Info
+        Text(text = "App Info", style = TextStyle(fontSize = 20.sp))
         Text(text = "App Name : ${iosInfo.appName}")
-        Text(text = "Bundle Id : ${iosInfo.bundleId}")
         Text(text = "App Version : ${iosInfo.appVersion}")
         Text(text = "App Short Version : ${iosInfo.appShortVersion}")
+        Text(text = "Bundle Id : ${iosInfo.bundleId}")
+        Spacer(modifier = Modifier.fillMaxWidth().height(height = 20.dp))
+
+        // Device Info
+        Text(text = "Device Info", style = TextStyle(fontSize = 20.sp))
         Text(text = "Name : ${iosInfo.name}")
         Text(text = "Model : ${iosInfo.model}")
         Text(text = "SystemName : ${iosInfo.systemName}")
         Text(text = "SystemVersion : ${iosInfo.systemVersion}")
         Text(text = "LocalizedModel : ${iosInfo.localizedModel}")
         Text(text = "IsPhysicalDevice : ${iosInfo.isPhysicalDevice}")
-        Text(text = "DeviceOrientation : ${iosInfo.deviceOrientation.getDeviceOrientation()}")
+        Text(text = "Orientation : ${iosInfo.deviceOrientation.getDeviceOrientation()}")
+        Spacer(modifier = Modifier.fillMaxWidth().height(height = 20.dp))
+
+        // Locale Info
+        Text(text = "Locale Info", style = TextStyle(fontSize = 20.sp))
+        Text(text = "Language Code : ${iosInfo.locale.languageCode}")
+        Text(text = "Region : ${iosInfo.locale.region}")
+        Spacer(modifier = Modifier.fillMaxWidth().height(height = 20.dp))
     }
 }
