7fd9d8db48610fccf0c2df878a8dc1f8268f1c09
Swapnil Musale <swapnilmusale19@gmail.com>
2025-04-13 02:32:23 +0530

    Test

--- sampleApp/composeApp/src/androidMain/kotlin/com/devx/kdeviceinfo/sample/App.android.kt
+++ sampleApp/composeApp/src/androidMain/kotlin/com/devx/kdeviceinfo/sample/App.android.kt
@@ -17,6 +17,7 @@ class AndroidApp : Application() {
 }
 
 class AppActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
